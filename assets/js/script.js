d29de6867143e562b0f35defabe41eac60cdde60
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2022-04-27 23:39:24 -0300

    feat: inclui função que gera a senha

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+let sliderElement = document.querySelector('.slider');
+let buttonElement = document.querySelector('.button');
+
+let sizePassword = document.querySelector('#size');
+let password = document.querySelector('.password');
+
+let containerPassword = document.querySelector('.container-password');
+
+let charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@';
+let newPassword = '';
+
+sizePassword.innerHTML = sliderElement.value;
+
+slider.oninput = function () {
+  sizePassword.innerHTML = this.value;
+};
+
+buttonElement.addEventListener('click', generatePassword);
+
+function generatePassword() {
+  let pass = '';
+  for (let i = 0, n = charset.length; i < sliderElement.value; i++) {
+    pass += charset.charAt(Math.floor(Math.random() * n));
+  }
+
+  containerPassword.classList.remove('hide');
+  password.innerHTML = pass;
+}
